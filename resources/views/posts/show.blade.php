7c9165f980cd68ff0849fe40277ade17cf7a804c
John Frail Bongat <jfraildev@gmail.com>
2019-06-23 17:25:36 +0800

    Added auth

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,10 +13,10 @@
 		Written on {{ $post->created_at }}
 	</small>
 	<hr>
-	<a href="/posts/{{ $post->id }}/edit" class="btn btn-outline-info">Edit</a>
+	<a href="/posts/{{ $post->id }}/edit" class="btn btn-success">Edit</a>
 
 	{!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right']) !!}
 		{!! Form::hidden('_method', 'DELETE') !!}
-		{!! Form::submit('DELETE', ['class' => 'btn btn-outline-danger']) !!}
+		{!! Form::submit('DELETE', ['class' => 'btn btn-danger']) !!}
 	{!! Form::close() !!}
 @endsection
